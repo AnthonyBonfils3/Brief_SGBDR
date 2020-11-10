a8be3a16c28d27cc59b7a155f2c9046f78292b5a
anthony <anthonybonfils3@gmail.com>
2020-11-10 16:59:31 +0100

    second commit

--- rendu_SGBDR.sql
+++ rendu_SGBDR.sql
@@ -101,7 +101,7 @@ SELECT NetflixShows.title FROM NetflixShows INNER JOIN NetflixTitle ON NetflixSh
 
 SELECT SUM(duration) FROM NetflixTitle WHERE NetflixTitle.type = 'Movie'
 
-"A creuser car la durée n'est pas mise en forme, ici on est en presence de string qu'on pourrait caster mais il y a aussi l'inité ->min"
+#"A creuser car la durée n'est pas mise en forme, ici on est en presence de string qu'on pourrait caster mais il y a aussi l'inité ->min"
 #SELECT duration, SUBSTR(duration, 1, 3), LENGTH(SUBSTR(duration, 1, 3))
 #FROM NetflixTitle WHERE NetflixTitle.type = 'Movie'
 
